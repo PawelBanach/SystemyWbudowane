1905a69095a180a335fa4a11193d8d412beb9ea9
michalkawalek <galaisius@gmail.com>
2017-01-07 01:37:06 +0100

    Added sending and receiving data from nrf24l01+

--- NRF24L01+/recv.py
+++ NRF24L01+/recv.py
@@ -1,9 +1,24 @@
+# -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 from lib_nrf24 import NRF24
 import time
 import spidev
 
+
+def toFloat(val):
+    try:
+        return float(val)
+    except:
+        print('received no int value')
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+redRGBValue = GPIO.PWM(24, 100)
+blueRGBValue = GPIO.PWM(20, 100)
+greenRGBValue = GPIO.PWM(21, 100)
 
 pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
 
@@ -28,22 +43,33 @@ radio.startListening()
 while True:
     ackPL = [1]
     while not radio.available(0):
-        time.sleep(1/100)
+        time.sleep(1/2)
 
     receivedMessage = []
     radio.read(receivedMessage, radio.getDynamicPayloadSize())
-    print("Received: {}".format(receivedMessage))
 
-    print("Translating the receivedMessage into unciode characters...")
     string = ""
     for n in receivedMessage:
         # Decode into standard unicode set
         if (n >= 32 and n <= 126):
             string += chr(n)
-    print(string)
+    print('received RGB setup:' + string)
+    colors = string.split(':')
+    print(colors)
+    redRGBColor = toFloat(colors[1])
+    greenRGBColor = toFloat(colors[3])
+    blueRGBColor = toFloat(colors[5])
+#    print(redRGBColor)
+#    print(greenRGBColor)
+#    print(blueRGBColor)
+    
+    redRGBValue.start(redRGBColor)
+    blueRGBValue.start(blueRGBColor)
+    greenRGBValue.start(greenRGBColor)
+    
 
     radio.writeAckPayload(1, ackPL, len(ackPL))
-    print("Loaded payload reply of {}".format(ackPL))
+#    print("Loaded payload reply of {}".format(ackPL))
     
 
 
--- NRF24L01+/sender.py
+++ NRF24L01+/sender.py
@@ -1,7 +1,36 @@
+# -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 from lib_nrf24 import NRF24
 import time
 import spidev
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, rc):
+    print('Connected with result code ' + str(rc))
+# Subscribing in on_connect() means that if we lose the connection and
+# reconnect then subscriptions will be renewed.
+    client.subscribe('home/rgbled')
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print 'Received message from mqtt server: Topic: ,' + msg.topic + 'Message: ' + str(msg.payload)
+    radio.write(msg.payload)
+    print("We sent the message of {}".format(msg.payload))
+    if radio.isAckPayloadAvailable():
+        returnedPL = []
+        radio.read(returnedPL, radio.getDynamicPayloadSize())
+        #print("Our returned payload was {}".format(returnedPL))
+    else:
+        #print("No payload received")
+        time.sleep(1)
+    
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect('localhost', 1883, 60)
+
 
 pipes = [ [0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2] ]
 
@@ -10,7 +39,7 @@ GPIO.setmode(GPIO.BCM)
 radio = NRF24(GPIO, spidev.SpiDev())
 radio.begin(0, 17)
 radio.setPayloadSize(32)
-radio.setChannel(0x60)
+radio.setChannel(0x70)
 
 radio.setDataRate(NRF24.BR_2MBPS)
 radio.setPALevel(NRF24.PA_MIN)
@@ -21,15 +50,11 @@ radio.enableAckPayload()
 radio.openWritingPipe(pipes[1])
 radio.printDetails()
 
-while True:
-    message = list("Hello World")
-    radio.write(message)
-    print("We sent the message of {}".format(message))
 
-    if radio.isAckPayloadAvailable():
-        returnedPL = []
-        radio.read(returnedPL, radio.getDynamicPayloadSize())
-        print("Our returned payload was {}".format(returnedPL))
-    else:
-        print("No payload received")
-        time.sleep(1)
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+
+
+client.loop_forever()
