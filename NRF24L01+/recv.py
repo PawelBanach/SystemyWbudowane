09aaf94c767d917806781ce04d30b694dae891cf
Yoshimori <pawel.banach@boostcom.no>
2017-01-16 22:11:24 +0100

    Add recv and sender

--- NRF24L01+/recv.py
+++ NRF24L01+/recv.py
@@ -1,9 +1,50 @@
+# -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 from lib_nrf24 import NRF24
 import time
 import spidev
+import os
+import time
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+temp_sensor = '/sys/bus/w1/devices/28-000008b7592f/w1_slave'
+
+def temp_raw():
+
+    f = open(temp_sensor, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+
+    lines = temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = temp_raw()
+
+    temp_output = lines[1].find('t=')
+    if temp_output != -1:
+        temp_string = lines[1].strip()[temp_output+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
+def toFloat(val):
+    try:
+        return float(val)
+    except:
+        print('received no float value')
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+redRGBValue = GPIO.PWM(24, 100)
+blueRGBValue = GPIO.PWM(20, 100)
+greenRGBValue = GPIO.PWM(21, 100)
 
 pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
 
@@ -28,22 +69,35 @@ radio.startListening()
 while True:
     ackPL = [1]
     while not radio.available(0):
-        time.sleep(1/100)
+        time.sleep(1/2)
 
     receivedMessage = []
     radio.read(receivedMessage, radio.getDynamicPayloadSize())
-    print("Received: {}".format(receivedMessage))
 
-    print("Translating the receivedMessage into unciode characters...")
     string = ""
     for n in receivedMessage:
         # Decode into standard unicode set
         if (n >= 32 and n <= 126):
             string += chr(n)
-    print(string)
+    print('received RGB setup:' + string)
+    colors = string.split(':')
+    print(colors)
+    redRGBColor = toFloat(colors[1])
+    greenRGBColor = toFloat(colors[3])
+    blueRGBColor = toFloat(colors[5])
+#    print(redRGBColor)
+#    print(greenRGBColor)
+#    print(blueRGBColor)
+    
+    redRGBValue.start(redRGBColor)
+    blueRGBValue.start(blueRGBColor)
+    greenRGBValue.start(greenRGBColor)
 
-    radio.writeAckPayload(1, ackPL, len(ackPL))
-    print("Loaded payload reply of {}".format(ackPL))
+    temperature = list(str(read_temp()))
+    temperature = [ord(elem) for elem in temperature]
+    print(temperature)
+    radio.writeAckPayload(1, temperature, len(temperature))
+#    print("Loaded payload reply of {}".format(ackPL))
     
 
 
--- NRF24L01+/sender.py
+++ NRF24L01+/sender.py
@@ -1,7 +1,55 @@
+# -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 from lib_nrf24 import NRF24
 import time
 import spidev
+import paho.mqtt.client as mqtt
+
+
+client = mqtt.Client("python_sub") # if it does not work, change it to parameterless constructor ...
+
+client.connect('localhost', 1883)
+
+publisher = mqtt.Client("python_pub")
+#publisher.connect('localhost', 1883)
+
+def toFloat(val):
+    try:
+        return float(val)
+    except:
+        print('received no float value')
+
+def on_connect(client, userdata, rc):
+    print('Connected with result code ' + str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe('home/rgbled')
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print 'Received message from mqtt server: Topic: ,' + msg.topic + 'Message: ' + str(msg.payload)
+    radio.write(msg.payload)
+    print("We sent the message of {}".format(msg.payload))
+    if radio.isAckPayloadAvailable():
+        returnedPL = []
+        radio.read(returnedPL, radio.getDynamicPayloadSize())
+
+        temperature = ""
+        for n in returnedPL:
+            if (n >= 32 and n <= 126):
+                temperature += chr(n)
+        
+        publisher.connect('localhost', 1883)
+        publisher.publish('home/temperature', toFloat(temperature))
+        publisher.disconnect()
+        print(toFloat(temperature))
+    else:
+        #print("No payload received")
+        time.sleep(1)
+    
+client.on_connect = on_connect
+client.on_message = on_message
+
 
 pipes = [ [0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2] ]
 
@@ -10,7 +58,7 @@ GPIO.setmode(GPIO.BCM)
 radio = NRF24(GPIO, spidev.SpiDev())
 radio.begin(0, 17)
 radio.setPayloadSize(32)
-radio.setChannel(0x60)
+radio.setChannel(0x70)
 
 radio.setDataRate(NRF24.BR_2MBPS)
 radio.setPALevel(NRF24.PA_MIN)
@@ -21,15 +69,11 @@ radio.enableAckPayload()
 radio.openWritingPipe(pipes[1])
 radio.printDetails()
 
-while True:
-    message = list("Hello World")
-    radio.write(message)
-    print("We sent the message of {}".format(message))
 
-    if radio.isAckPayloadAvailable():
-        returnedPL = []
-        radio.read(returnedPL, radio.getDynamicPayloadSize())
-        print("Our returned payload was {}".format(returnedPL))
-    else:
-        print("No payload received")
-        time.sleep(1)
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+
+
+client.loop_forever()
